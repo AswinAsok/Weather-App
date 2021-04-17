494c016896bfa1b8416bf82f6fba3528ef53d170
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-17 18:50:01 +0530

    Rewritten Display.jsx

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -1,35 +1,26 @@
-import React from "react";
-import "./Display.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Display = ({ location, search, setSearch }) => {
   const [weather, setWeather] = useState({});
-  const [checkdata, setCheckdata] = useState(false);
+  const [error, setError] = useState("");
 
-
-  const fetchweather = () => {
-
-    fetch(`http://api.github.com/users/AswinAsok`)
-      .then((res) => res.json())
-      .then((data) => {
-        setWeather(data);
-        setCheckdata(true);
-        if(data.message){
-          setCheckdata(false);
-        }
-      });
+  const fetchdetails = () => {
+    if (search && location.length > 0) {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e872642d325a2c265cecf7fac7986a27`)
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data);
+          if(data.message) {
+            setError(data.message);
+          }
+        });
+    }
   };
 
   return (
-    <div className="">
-      {(() => {
-        if (search && !checkdata) {
-          return <div>
-              {fetchweather()}
-              {console.log(weather)}
-          </div>;
-        }
-      })()}
+    <div>
+      {fetchdetails()}
+      {console.log(weather)}
     </div>
   );
 };
