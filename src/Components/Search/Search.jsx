8447d84bb97bc761996961ad14d57f869d226dbb
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-17 10:42:46 +0530

    Seach Component Styling done

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import './Search.css'
 
 const Search = () => {
   return (
-    <div>
-      <TextField
-        id="outlined-full-width"
-        label="Search Weather"
-        style={{marginLeft:"25%", marginTop:"2.5%", width: "50%" }}
-        placeholder="Location"
-        margin="normal"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        variant="outlined"
-      />
+    <div className="Search-container">
+      <Grid spacing={3}>
+        <TextField
+          id="outlined-full-width"
+          label="Search Weather"
+          style={{marginTop: "10%", width: "70%" }}
+          placeholder="Location"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
+
+        <Button variant="outlined" color="primary" style={{ marginTop: "10%", marginLeft: "5px", padding:"13px"}} size="large">
+          Search
+        </Button>
+      </Grid>
     </div>
   );
 };
