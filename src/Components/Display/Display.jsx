3bbe0d011197d6be08d5f8855509cd9756823320
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-18 13:41:16 +0530

    Icon got loaded successfully but i am not satisfied with the quality

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -6,11 +6,11 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,6 +40,7 @@ const Display = ({ search, weather, location, error }) => {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
+    console.log(`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`)
     setExpanded(!expanded);
   };
 
@@ -49,87 +50,109 @@ const Display = ({ search, weather, location, error }) => {
         if (search && location.length > 0 && error == "false") {
           return (
             <div>
-              <Card className={classes.root}>
-                <CardContent>
-                  <h2>{weather.weather[0].main}</h2>
-                </CardContent>
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                <div>
+                  <Card className={classes.root}>
+                    <CardMedia
+                      className={classes.media}
+                      image={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                      title="Paella dish"
+                    />
 
-                <CardActions disableSpacing>
-                  {Math.round(weather.main.temp)}°C
-                  <IconButton
-                    className={clsx(classes.expand, {
-                      [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </CardActions>
+                    <CardContent>
+                      <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                      >
+                        <h1>{weather.weather[0].main}</h1>
+                      </Grid>
+                    </CardContent>
 
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Grid
-                      container
-                      direction="row"
-                      justify="space-around"
-                      alignItems="center"
-                    >
-                      <div>
-                        Feels like: <b>{weather.main.feels_like}</b>
-                      </div>
-                    </Grid>
-                    <br></br>
-                    <Divider />
-                    <br></br>
-                    <Grid
-                      container
-                      direction="row"
-                      justify="space-around"
-                      alignItems="center"
-                    >
-                      <div>
-                        Humidity:<b>{weather.main.humidity}%</b>
-                      </div>
-                      <div>
-                        Pressure:<b>{Math.round(weather.main.pressure)}mb</b>
-                      </div>
-                    </Grid>
-                    <br></br>
-                    <Divider />
-                    <br></br>
-                    <Grid
-                      container
-                      direction="column"
-                      justify="space-around"
-                      alignItems="center"
-                    >
-                      <div>
-                        Visiblity: <b>{weather.visibility / 1000}km</b>
-                      </div>
-                    </Grid>
-                    <br></br>
-                    <Divider />
-                    <br></br>
-                    
-                    <Grid
-                      container
-                      direction="row"
-                      justify="space-around"
-                      alignItems="center"
-                    >
-                      
-                      <div>
-                        Wind: <b>{weather.wind.speed}m/s</b>
-                      </div>
-                      <div>
-                        Wind Direction: <b>{weather.wind.deg}deg</b>
-                      </div>
-                    </Grid>
-                  </CardContent>
-                </Collapse>
-              </Card>
+                    <CardActions disableSpacing>
+                      Temperature:&nbsp;<b>{weather.main.temp}</b>°C
+                      <IconButton
+                        className={clsx(classes.expand, {
+                          [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      >
+                        <ExpandMoreIcon />
+                      </IconButton>
+                    </CardActions>
+
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                      <CardContent>
+                        <Grid
+                          container
+                          direction="row"
+                          justify="space-around"
+                          alignItems="center"
+                        >
+                          <div>
+                            Feels like: <b>{weather.main.feels_like}°C</b>
+                          </div>
+                        </Grid>
+                        <br></br>
+                        <Divider />
+                        <br></br>
+                        <Grid
+                          container
+                          direction="row"
+                          justify="space-around"
+                          alignItems="center"
+                        >
+                          <div>
+                            Humidity: <b>{weather.main.humidity}%</b>
+                          </div>
+                          <div>
+                            Pressure:{" "}
+                            <b>{Math.round(weather.main.pressure)}mb</b>
+                          </div>
+                        </Grid>
+                        <br></br>
+                        <Divider />
+                        <br></br>
+                        <Grid
+                          container
+                          direction="column"
+                          justify="space-around"
+                          alignItems="center"
+                        >
+                          <div>
+                            Visiblity: <b>{weather.visibility / 1000}km</b>
+                          </div>
+                        </Grid>
+                        <br></br>
+                        <Divider />
+                        <br></br>
+
+                        <Grid
+                          container
+                          direction="row"
+                          justify="space-around"
+                          alignItems="center"
+                        >
+                          <div>
+                            Wind: <b>{weather.wind.speed}m/s</b>
+                          </div>
+                          <div>
+                            Wind Direction: <b>{weather.wind.deg}deg</b>
+                          </div>
+                        </Grid>
+                      </CardContent>
+                    </Collapse>
+                  </Card>
+                </div>
+              </Grid>
             </div>
           );
         }
